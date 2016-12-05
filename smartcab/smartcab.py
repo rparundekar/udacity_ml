79a40be3baa85a95a4038fb437eda41cecd2788d
rparundekar <rparundekar@gmail.com>
2016-12-05 08:18:04 -0800

    Added log files

--- smartcab/smartcab.py
+++ smartcab/smartcab.py
@@ -3,3 +3,9 @@ import visuals as vs
 
 # Pretty display for notebooks
 %matplotlib inline
+
+vs.plot_trials('sim_no-learning.csv')
+
+vs.plot_trials('sim_default-learning.csv')
+
+vs.plot_trials('sim_improved-learning.csv')
