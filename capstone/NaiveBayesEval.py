53b5bb284f5e4da8f666e079eb6709b8700779be
rparundekar <rparundekar@us.toyota-itc.com>
2017-03-31 16:08:53 -0700

    feat: Implemented the partial_fit training for MultinomialNB
    Initially, the file size being 22GB for training, we could not load it for training the NB Classifier. By using the partial_fit function, we overcame this limitation.

--- capstone/NaiveBayesEval.py
+++ capstone/NaiveBayesEval.py
@@ -1,37 +1,79 @@
 import pandas as pd
-print("Reading X")
-dataX=pd.read_csv('/Users/rparundekar/dataspace/dbpedia2016/datasetX.csv')
-dataX.head()
-print("Reading Y")
-dataY=pd.read_csv('/Users/rparundekar/dataspace/dbpedia2016/datasetY.csv')
-dataY.head()
-print("Done Reading")
 
-del dataX['id']
-del dataY['id']
+print("Reading Header Y")
+headerY=pd.read_csv('/Users/rparundekar/dataspace/dbpedia2016/headerY.csv')
 
 import numpy as np
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(dataX, dataY, test_size=0.20, random_state=42)
-
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import confusion_matrix
 
-conf = [[0,0],[0,0]];
+#Folder for the dataset
+datasetFolder = '/Users/rparundekar/dataspace/dbpedia2016/dataset/'
+#Number of files
+numberOfFiles = 52
+#Test split
+testSplit=0.25
 
-for classY in y_train:
-    print("Learning & Testing for " + classY )
-    y_train_this=y_train[classY]
-    y_test_this=y_test[classY]
-    clf = MultinomialNB()
-    clf.fit(X_train, y_train_this)
-    y_pred=clf.predict(X_test)
-    conf = conf + confusion_matrix(y_test_this, y_pred)
-    print (conf)
+#Randomize the list of numbers so we can split train and test dataset
+listOfFiles=list(range(1,numberOfFiles+1))
+import random
+random.shuffle(listOfFiles)
+splitIndex=int((1-testSplit)*numberOfFiles)
+
+confusionMatrix = [[0,0], [0,0]]
+
+#Iterate through each class for creating the NB for that class
+for index, row in headerY.iterrows():
+    classId=row[0]
+    classY=row[1]
+    if(classId=='id'):
+        continue
+    print("Learning for " + classId )
+    #Initialize the classifier
+    classifier = MultinomialNB()
+    for trainIndex in range(0,splitIndex):
+        print('Reading X for file datasetX_{}.csv'.format(listOfFiles[trainIndex]))
+        dataX=pd.read_csv(datasetFolder + 'datasetX_{}'.format(listOfFiles[trainIndex]) + '.csv')
+        
+        print('Reading Y for file datasetY_{}.csv'.format(listOfFiles[trainIndex]))
+        dataY=pd.read_csv(datasetFolder + 'datasetY_{}'.format(listOfFiles[trainIndex])  + '.csv')
+        
+        del dataX['id']
+        del dataY['id']
+
+        y_train_this=dataY[classY]
+        print('Updating model')
+        classifier.partial_fit(dataX, y_train_this, classes=np.unique(y_train_this))
     
-tp = (float)(conf[1,1])
-fp = (float)(conf[0,1])
-fn = (float)(conf[1,0])
+    currentConfusionMatrix = [[0,0], [0,0]]
+    print("Testing for " + classId)
+    for testIndex in range(splitIndex,numberOfFiles):
+        print("Reading X " + listOfFiles[testIndex])
+        dataX=pd.read_csv(datasetFolder + 'datasetX_' + listOfFiles[testIndex] + '.csv')
+        
+        print("Reading Y" + listOfFiles[testIndex])
+        dataY=pd.read_csv(datasetFolder + 'datasetY_' + listOfFiles[testIndex] + '.csv')
+        
+        del dataX['id']
+        del dataY['id']
+
+        y_test_this=dataY[classY]
+        print('Predicting Y')
+        y_pred = clf.predict(dataX)
+        currentConfusionMatrix = currentConfusionMatrix + confusion_matrix(y_test_this, y_pred)
+    
+    print('Class ' + classId + ' confusion matrix: ')
+    print(currentConfusionMatrix)   
+    confusionMatrix=confusionMatrix+currentConfusionMatrix
+    
+print('Complete confusion matrix: ')
+print(confusionMatrix)   
+
+
+
+tp = (float)(confusionMatrix[1,1])
+fp = (float)(confusionMatrix[0,1])
+fn = (float)(confusionMatrix[1,0])
 precision = tp/(tp+fp)
 recall = tp/(tp+fn)
 f1score = 2.0 * precision * recall / (precision+recall)
